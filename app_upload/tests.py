c43ee023ee01b1450ff2a751c39e2d9162b30053
mahdi <syadtym983@gmail.comm>
2022-02-10 11:44:30 +0330

    making_yml_file

--- app_upload/tests.py
+++ app_upload/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Flile
+
+
+class TestFileModel(TestCase):
+
+    def setUp(self) -> None:
+        Flile.objects.create(body="user/files/facade_BGUvUU3.md",name="test")
+
+    def test_file_size(self):
+        file = Flile.objects.get(name="test")
+        self.assertEqual(file.set_file_size(),file.body.size,"size are not ==")
+
+
